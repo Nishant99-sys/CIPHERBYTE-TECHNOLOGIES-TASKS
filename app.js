3a2fbb8bd7db5a0a0eb741cc210c46be16a1aa58
Nishant kumawat <nishantkumawat002@gmail.com>
2024-07-22 22:50:51 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,50 @@
+var Typed = new Typed(".text",{
+    strings:["Frontend Developer...", "Youtuber...", "Web Developer..."],
+    typespeed: 100,
+    backspeed: 100,
+    backDelay: 1000,
+    loop: true
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
